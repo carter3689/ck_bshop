327564039179bcf4e1d13f314d888b1e0af7b348
joel.carter <joel.carter@waterfurnace.com>
2024-05-08 06:36:43 -0500

    Initial Commit

---
+++ CrownKings_SwiftUI/CrownKings_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrownKings_SwiftUIApp.swift
+//  CrownKings_SwiftUI
+//
+//  Created by Joel Carter on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CrownKings_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
